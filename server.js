0dbea632c077121a31e5b31f4d96d52a719dff50
William Kolean <william.kolean@rga.com>
2022-07-04 10:51:16 +0900

    refactor for posting to github

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var express = require('express');
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('src'));
 app.use(express.static('/tmp'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/src/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
